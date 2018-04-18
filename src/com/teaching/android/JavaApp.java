57ed0978c1754cdc6f0ac711bf3909e9433516f5
MEERY\mery <sergio.93.oc@gmail.com>
2018-04-18 12:26:50 +0200

    Acertar numero al azar diciendo mayor o menor con bucle

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -50,10 +50,41 @@ public class JavaApp {
             System.out.println("No se ha podido crear un lector de flujo del teclado o insertado una letra");
         }
         */
-        int j=0;
-        for (int i=1;i<=20;i++){
-            System.out.println(i+"º numero par es: "+j);
-            j=j+2;
+        try {
+            Scanner leer = new Scanner(System.in);
+            Random azar= new Random();
+            int numeroAleatorio= azar.nextInt(10);
+            int contador = 0;
+            int numeroElegido;
+            System.out.println("Numero acertar es "+numeroAleatorio);
+            do{
+                System.out.println("Selecciona el numero para ver si aciertas");
+                numeroElegido=leer.nextInt();
+                if(numeroElegido==numeroAleatorio){
+                    System.out.println("Acertaste!!!");
+                    contador=3;
+                }else{
+                    if(numeroElegido>numeroAleatorio){
+                        if(contador<3) {
+                            contador++;
+                            System.out.println("Numero es menor y fallaste!!");
+                        }else{
+                            System.out.println("Te quedaste sin intentos");
+                            break;
+                        }
+                    }else{
+                        contador++;
+                        if(contador<3) {
+                            System.out.println("Numero es mayor y fallaste!!");
+                        }else{
+                            System.out.println("Te quedaste sin intentos");
+                            break;
+                        }
+                    }
+                }
+            }while(contador<3);
+        }catch (Exception e){
+            System.out.println("No se ha podido leer el flujo del teclado o has escrito una letra");
         }
         /*
 
